dfd4b3996fd0ad95f8b9238dd788cf9bb7b1d26d
Mili Jolly <66016518+MiliJolly@users.noreply.github.com>
2024-11-24 16:46:34 +0000

    Integrated expense graph into telebot code

--- telebot_code/expense_graph.py
+++ telebot_code/expense_graph.py
@@ -2,6 +2,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from datetime import datetime
+import add_group_exp.read_json as read_json
+import db_operations
+import ast
+from telebot import types
 
 def run(message, bot):
     markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
@@ -13,17 +17,94 @@ def run(message, bot):
 def monthwise_or_daywise_record(message, bot):
     if message.text == "Daywise":
         gran = 'day'
-    else:
+        msg = bot.reply_to(message, "Enter the number of "+gran+"s you want to show on the graph")
+        bot.register_next_step_handler(msg, vis_graph_record, bot, gran)
+    elif message.text == 'Monthwise':
         gran ='month'
-    
+        msg = bot.reply_to(message, "Enter the number of "+gran+"s you want to show on the graph")
+        bot.register_next_step_handler(msg, vis_graph_record, bot, gran)
+    else:
+        msg = bot.reply_to(message, "Sorry, did not recognize the option. Try again.")
+        bot.register_next_step_handler(msg, run, bot)
+
+def vis_graph_record(message, bot, granularity):
+    txt = message.text
+    chid = message.chat.id
+    if not txt.isnumeric():
+        msg = bot.reply_to(message, "Sorry, please enter only a whole number. Enter the number of "+granularity+"s you want to show on the graph")
+        bot.register_next_step_handler(msg, vis_graph_record, bot, gran)
+    else:
+        num_ele = int(txt)
+        markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+        markup.add("Mine")
+        markup.add("Group")
+        msg = bot.reply_to(message, "Do you want to plot your expenses or group expenses?", reply_markup = markup)
+        bot.register_next_step_handler(msg, single_or_group_expenses, bot, gran, num_ele)
+
+def single_or_group_expenses(message, bot, granularity, ndays):
+    chid = message.chat.id
+    uid = message.from_user.id
+    if message.text == 'Mine':
+        dfp = []
+        det = db_operations.read_user_transaction(uid)['transactions']['expense_data']
+        if det == None:
+            bot.send_message(chid, "No expenses made yet")
+        else:
+            exp = det.transactions['expense_data']
+            for rec in exp:
+                record = {'userid' : uid, 'timestamp' : rec['date'], 'expense' : rec['amount']}
+                dfp.append(record)
+            df = pd.DataFrame(dfp)
+            F = plot_single_user_expenses(df, uid, granularity=granularity, ndays=ndays)
+            bot.send_message(chid, "Here is your timeline of your expenses:")
+            bot.send_photo(chid, F)
+    elif message.text == 'Group':
+        det = read_json()
+        if det == {} or not (uid in det.keys()):
+            bot.send_message(chid, "No group expenses made yet")
+        else:
+            df = conv_to_df(det[uid]['expense'])
+            F = plot_expenses_with_histogram(df, granularity=granularity, ndays=ndays)
+            bot.send_message(chid, "Here is your timeline of your group expenses:")
+            bot.send_photo(chid, F)
+    else:
+        markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+        markup.add("Mine")
+        markup.add("Group")
+        msg = bot.reply_to(message, "Only 2 options are available. Try again.\nDo you want to plot your expenses or group expenses?", reply_markup = markup)
+        bot.register_next_step_handler(msg, single_or_group_expenses, bot, granularity, ndays)
+
+def conv_to_df(det):
+    dfp = []
+    for record in det:
+        i1 = record.find('[')
+        ml = record[i1:]
+        member_list = ast.literal_eval(ml)
+        member_list.append("You")
+        expensedata = record[0:i1-1].split(",")
+        date = expensedata[0]
+        dollaramt = expensedata[1]
+        precord = {'timestamp' : date, 'expense': dollaramt, 'name' : 'You'}
+        dfp.append(precord)
+        for name in member_list:
+            pcopy = precord.copy()
+            pcopy['name'] = name
+            dfp.append(pcopy)
+    final_rec = pd.DataFrame(dfp)
+    return final_rec
 
 def plot_expenses_with_histogram(df, granularity='day', ndays=0):
     users = df['name'].unique()
-    fig, ax1 = plt.subplots(figsize=(12, 6))
-
+    fig, ax1 = plt.subplots(figsize=(12, 6))\
+    
     for user in users:
         user_data = df[df['name'] == user]
-        ax1.plot(user_data['timestamp'], user_data['expense'], label=user, marker='o')
+        L = len(user_data['timestamp'])
+        if ndays == 0:
+            ndays = L
+        l = L - ndays
+
+        ax1.plot(user_data['timestamp'][l:], user_data['expense'][l:], label=user, marker='o')
 
     ax1.xaxis.set_major_locator(mdates.DayLocator() if granularity == 'day' else mdates.MonthLocator())
     ax1.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d' if granularity == 'day' else '%Y-%m'))
@@ -49,8 +130,9 @@ def plot_expenses_with_histogram(df, granularity='day', ndays=0):
     ax1.legend(loc='upper left')
     plt.title(f"Expenses ({granularity.capitalize()}wise) per User with Total Expenses")
     plt.savefig("temp.png")
+    return open("temp.png", "rb")
 
-def plot_single_user_expenses(df, userid, granularity='day'):
+def plot_single_user_expenses(df, userid, granularity='day', ndays=0):
     # Filter data for the specified user
     user_data = df[df['userid'] == userid]
 
@@ -61,7 +143,11 @@ def plot_single_user_expenses(df, userid, granularity='day'):
     fig, ax1 = plt.subplots(figsize=(12, 6))
 
     # Plot user expenses
-    ax1.plot(user_data['timestamp'], user_data['expense'], label=f"User {userid}", marker='o')
+    L = len(user_data['timestamp'])
+    if ndays == 0:
+        ndays = L
+    l = L - ndays
+    ax1.plot(user_data['timestamp'][l:], user_data['expense'][l:], label=f"User {userid}", marker='o')
 
     # Format x-axis
     ax1.xaxis.set_major_locator(mdates.DayLocator() if granularity == 'day' else mdates.MonthLocator())
@@ -76,3 +162,4 @@ def plot_single_user_expenses(df, userid, granularity='day'):
     # Add legend and show plot
     ax1.legend(loc='upper left')
     plt.savefig("temp.png")
+    return open("temp.png", "rb")
